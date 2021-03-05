8feeb2adfe6160f06ede404c80579904c6868391
Alexey Sobolev <sobolev@mail.de>
2021-03-05 23:08:13 +0100

    Fix progress bar

--- src/components/Progress.tsx
+++ src/components/Progress.tsx
@@ -4,8 +4,9 @@ import { AppContext } from '../context/globalState';
 import { AppContextInterface } from '../models/context';
 
 const ProgressBar = () => {
-    const { currentQuestion, data }: AppContextInterface = useContext(AppContext);
-    const percent = Math.round(currentQuestion / data?.results?.length * 100);
+    const { data, results }: AppContextInterface = useContext(AppContext);
+    const percent = Math.round(results.length / data?.results?.length * 100);
+
     return (
         <div className="progress-bar">
             <Progress percent={percent} />
