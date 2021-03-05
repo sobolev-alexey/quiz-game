f51da5d0b821b017c9ff2f61c82a56ee4bdcb89e
Alexey Sobolev <sobolev@mail.de>
2021-03-05 23:09:50 +0100

    Add strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,9 @@ import App from './App';
 import reportWebVitals from './utils/reportWebVitals';
 
 ReactDOM.render(
-  <App />,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
