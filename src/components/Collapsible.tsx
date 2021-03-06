2a47f16c58e85b6afd9a419c186d5148a5ce5d7b
Alexey Sobolev <sobolev@mail.de>
2021-03-06 20:29:22 +0100

    Update results list

--- src/components/Collapsible.tsx
+++ src/components/Collapsible.tsx
@@ -4,27 +4,35 @@ import { Collapse } from 'antd';
 import { CaretRightOutlined } from '@ant-design/icons';
 import { ResultsInterface } from '../models/context';
 
-const Collapsible = ({ results }: { results: ResultsInterface[] }) => (
-  <div>
-    <Collapse
-      bordered={false}
-      className="site-collapse-custom-collapse"
-      expandIcon={({ isActive }) => <CaretRightOutlined rotate={isActive ? 90 : 0} />}
-    >
-      {
-        results?.map(({ question, answer, correct }, index) => (
-          <Collapse.Panel header={sanitizeHtml(question)} key={index}>
-            <div>
-              <p>
-                You answered {correct ? ' correctly' : ' incorrectly'}: {answer}
-              </p>
-              {!correct && <p>Correct answer: {correct}</p>}
-            </div>
-          </Collapse.Panel>
-        ))
-      }
-    </Collapse>
-  </div>
-);
+const Collapsible = ({ results }: { results: ResultsInterface[] }) => {
+  const getHeader = (title: string, isAnsweredCorrectly?: boolean) => (
+    <div className={`answer-${isAnsweredCorrectly?.toString()}`}>
+      {title}
+    </div>
+  );
+  
+  return (
+    <div>
+      <Collapse
+        bordered={false}
+        className="site-collapse-custom-collapse"
+        expandIcon={({ isActive }) => <CaretRightOutlined rotate={isActive ? 90 : 0} />}
+      >
+        {
+          results?.map(({ question, answer, correct, isAnsweredCorrectly }, index) => (
+            <Collapse.Panel header={getHeader(sanitizeHtml(question), isAnsweredCorrectly)} key={index}>
+              <div>
+                <p>
+                  You answered {isAnsweredCorrectly ? ' correctly' : ' incorrectly'}: {answer}
+                </p>
+                {!isAnsweredCorrectly && <p>Correct answer: {correct}</p>}
+              </div>
+            </Collapse.Panel>
+          ))
+        }
+      </Collapse>
+    </div>
+  );
+}
 
 export default Collapsible;
