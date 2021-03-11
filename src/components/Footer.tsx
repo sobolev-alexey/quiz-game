d07a6c6c66b50c6128ff300f6691b7079d4f8f59
Alexey Sobolev <sobolev@mail.de>
2021-03-11 18:57:38 +0100

    Added image attribution

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,22 @@
 const Footer = () => (
     <footer className="footer">
-        <a
+      <a
         href="https://lexer.dev"
         target="_blank"
         rel="noopener noreferrer"
       >
         © Alexey Sobolev (https://lexer.dev)
       </a>
+      <div>
+        Images designed by
+        <a
+          href="https://www.freepik.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        Freepik
+        </a>
+      </div>
     </footer>
 );
 
