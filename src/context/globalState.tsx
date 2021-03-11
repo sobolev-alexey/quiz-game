fbcac561613328ce93857342265a0d33f590ee07
Alexey Sobolev <sobolev@mail.de>
2021-03-11 18:17:06 +0100

    Send request to backend proxy, get encrypted values

--- src/context/globalState.tsx
+++ src/context/globalState.tsx
@@ -1,4 +1,7 @@
-import React, { createContext, useState, useEffect, ReactNode } from 'react';
+import { createContext, useState, useEffect, ReactNode } from 'react';
+import axios from 'axios';
+import { generateKeys, privateDecrypt } from '../utils/encryption';
+import { IKeys } from '../models/context';
 
 export const AppContext = createContext<any>({})
 
@@ -9,28 +12,48 @@ const GlobalState = ({ children }: { children: ReactNode }) => {
 	const [results, setResults] = useState([]);
 	const [currentQuestion, setCurrentQuestion] = useState(0);
 	const [difficulty, setDifficulty] = useState('');
+	const [keys, setKeys] = useState<IKeys>();
+
+	useEffect(() => {
+		try {
+			const keys: IKeys = generateKeys();
+			setKeys(keys);
+		} catch (error) {
+			console.error(error);
+		}
+	}, []);
 
 	useEffect(() => {
 		try {
 			const onNewGame = async () => {
 				setLoading(true);
-				fetch(`https://opentdb.com/api.php?amount=10&type=boolean&difficulty=${difficulty}`)
-					.then((res) => res.json())
-					.then((data) => {
-						setData(data);
-						setLoading(false);
-						setNewGame(false);
-						setResults([]);
-						setCurrentQuestion(0);
-					});
+
+				const headers = {
+					"Content-Type": "application/json"
+				};
+				const url = 'https://us-central1-quiz-game-challenge.cloudfunctions.net/quiz';
+				const payload = {
+					difficulty,
+					key: keys?.publicKey || '',
+				}
+				const response = await axios.post(url, payload, { headers });
+				if (response?.data) {
+					const decrypted = privateDecrypt(keys?.privateKey || '', response?.data?.encrypted);
+					const results = decrypted?.results;
+					setData({ results });
+					setLoading(false);
+					setNewGame(false);
+					setResults([]);
+					setCurrentQuestion(0);
+				}	
             }
 
-			newGame && onNewGame();
+			keys?.publicKey && newGame && onNewGame();
 		} catch (error) {
 			console.error(error);
 		}
 
-	}, [newGame, difficulty]);
+	}, [newGame, difficulty, keys?.publicKey]);
 
 	return (
 		<AppContext.Provider value={{
